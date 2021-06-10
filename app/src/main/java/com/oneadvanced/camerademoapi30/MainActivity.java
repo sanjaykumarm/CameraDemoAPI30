10d60c6b05b8af0bb4f4957d8ec12e5c3918de7b
Sanjaykumar Modhawadiya <sanjaykumar.modhawadiya@oneadvanced.com>
2021-06-10 16:06:36 +0530

    Gitaction2

--- app/src/main/java/com/oneadvanced/camerademoapi30/MainActivity.java
+++ app/src/main/java/com/oneadvanced/camerademoapi30/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onActivityResult(ActivityResult result) {
                         if (result.getResultCode() == Activity.RESULT_OK) {
-                            // There are no request codes here in new method
+                            // There are no request codes here
                             Intent data = result.getData();
                             moveImage();
                         }
